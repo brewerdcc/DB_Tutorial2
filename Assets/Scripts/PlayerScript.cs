9fb9193ed829a3d0274e42a9d6111087005c80ce
brewerdcc <daniellebrewer@knights.ucf.edu>
2021-03-11 21:15:09 -0500

    Challenge commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public float jumpForce;
+    
+    public Text score;
+
+    public Text jumpText;
+
+    public Text hozText;
+
+    private int scoreValue = 0;
+
+    private bool facingRight = true;
+
+    private bool isOnGround;
+
+    public Transform groundcheck;
+
+    public float checkRadius;
+
+    public LayerMask allGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip ();
+        }
+
+        else if (facingRight == true && hozMovement < 0)
+
+        {
+            Flip();
+
+        }
+
+        if (hozMovement > 0 && facingRight == true)
+        {
+            Debug.Log ("Facing Right");
+            hozText.text = "Facing Right";
+        }
+                if (hozMovement < 0 && facingRight == false)
+        {
+            Debug.Log ("Facing Left");
+            hozText.text = "Facing Left";
+        }
+
+        if (vertMovement > 0 && isOnGround == false)
+        {
+            Debug.Log ("Jumping");
+            jumpText.text = "Jumping";
+        }
+
+        else if (vertMovement == 0 && isOnGround == true)
+        {
+            Debug.Log ("Not Jumping");
+            jumpText.text = "Not Jumping";
+        }
+    }
+        void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
